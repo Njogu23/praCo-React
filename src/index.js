f83a2c9c439d5681dfe1177cf0dc1c6f8ff278f9
George Njogu <sbeveme@gmail.com>
2022-10-05 17:30:31 +0300

    Home and Workout pages added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from "react-router-dom";
 import App from './components/App'
+import Workout from './pages/Workout'
 
 
 ReactDOM.render(
   <BrowserRouter>
+    <Route path="/workouts">
+      <Workout />
+    </Route>
     <Route exact path="/">
       <App />
     </Route>
