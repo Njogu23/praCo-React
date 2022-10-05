26b818a293b9cec613a349d2b8ab8a351bfe8e10
George Njogu <sbeveme@gmail.com>
2022-10-05 18:11:23 +0300

    NavBar removed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
 import React from "react";
-import NavBar from "../components/NavBar";
 import SearchBar from "../components/SearchBar";
 
 const Home = () => {
     return (
         <div style={{background:"indigo"}}>
-          <NavBar />
+          {/* <NavBar /> */}
           <header style={{textAlign:'center'}}>
             <h1 style={{color:'white', fontSize:"100px"}}>praCo.</h1>
           </header>
