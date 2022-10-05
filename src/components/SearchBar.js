93be0b955ac4e546327e0649d51288b7bef2a829
George Njogu <sbeveme@gmail.com>
2022-10-05 12:24:13 +0300

    SearchBar component created

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+      <div style={{padding:"15px", textAlign:"center"}}>
+        <form>
+          <input type="search" placeholder="search" style={{fontSize:"25px"}} ></input>
+        </form>
+      </div>
+      
+    )
+  }
+
+export default SearchBar;
