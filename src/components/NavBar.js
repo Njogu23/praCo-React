83b3e34c1e5dd1beb48acc8eabed26df29ce8cc8
George Njogu <sbeveme@gmail.com>
2022-10-05 22:37:47 +0300

    NavLinks styled

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 const NavBar = () => {
     return (
-      <div style={{display:"flex", padding:"2px", justifyContent:"center", fontFamily:"'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif"}}>
-       <h4 style={{padding:"15px"}}>Home</h4>
-       <h4 style={{padding:"15px"}}>Workouts</h4>
-      </div>
+      <nav style={{display:"flex", justifyContent:"center" }}>
+       <NavLink  exact to="/" >Home</NavLink>
+       <NavLink to="/workouts" >Workouts</NavLink>
+       <NavLink to="/challenge" >Challenge</NavLink>
+      </nav>
       
     )
   }
