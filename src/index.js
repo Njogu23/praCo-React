c0932ca4232281c9a75818ec3b95d59d6e72f96d
George Njogu <sbeveme@gmail.com>
2022-10-05 12:30:24 +0300

    react-router-dom installed and Browser Router imported

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from "react-router-dom";
 import App from './components/App'
 
 
 ReactDOM.render(
-  <App />, document.getElementById("root")
+  <BrowserRouter>
+    <Route exact path="/">
+      <App />
+    </Route>
+  </BrowserRouter>,
+   document.getElementById("root")
 )
