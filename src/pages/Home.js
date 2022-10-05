0da699d1a5b2c567d438ef737fccdeb72f04cfa5
George Njogu <sbeveme@gmail.com>
2022-10-05 18:08:42 +0300

    header added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,9 @@ const Home = () => {
     return (
         <div style={{background:"indigo"}}>
           <NavBar />
+          <header style={{textAlign:'center'}}>
+            <h1 style={{color:'white', fontSize:"100px"}}>praCo.</h1>
+          </header>
           <SearchBar />
         </div>
     )
