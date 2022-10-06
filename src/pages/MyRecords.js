608de5a64b34855816bf5e4ad7ae397bb3c0ce3f
George Njogu <sbeveme@gmail.com>
2022-10-06 13:41:15 +0300

    Init MyRecords Page

---
+++ src/pages/MyRecords.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const MyRecords = () => {
+    return (
+        <table className="table table-bordered">
+            <thead>
+                <tr>
+                    <th >
+                        <h3>Date</h3>
+                    </th>
+                    <th >
+                        <h3>Exercise</h3>
+                    </th>
+                    <th >
+                        <h3>Sets</h3>
+                    </th>
+                    <th >
+                        <h3>Reps</h3>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    )
+}
+
+export default MyRecords;
