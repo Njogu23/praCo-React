8f503700af3e805bf60c71320bdf68de7a853b97
George Njogu <sbeveme@gmail.com>
2022-10-06 03:15:16 +0300

    target workouts done

--- src/components/BodyPartList.js
+++ src/components/BodyPartList.js
@@ -1,13 +1,37 @@
 import React, {useEffect, useState} from "react";
 
-const BodyPartsList = () => { 
+
+const BodyPartsList = ({workouts}) => { 
     const [bodyParts, setBodyParts] = useState([])
-  
+    const [targetWorkouts, setTargetWorkouts] = useState("")
+
+    const filterByBodyPart = workouts.filter(item => item.bodyPart === targetWorkouts)
+    
+    
+    const workout = filterByBodyPart.map(item => {
+      return (
+        <section key={item.id} style={{padding:"2px", border:"solid", borderRadius:"8px"}}>
+          <h2>{item.name}</h2>
+          <img src={item.gifUrl} alt={item.name}></img>
+          <p>target muscle : {item.target}</p>
+          <p>equipment : {item.equipment}</p>
+        </section>
+      )
+    })
+
+    const handleWorkouts = (e) => {
+      setTargetWorkouts(e.target.textContent)
+     
+    }
+
+    const handdleDelete = (e) => {
+      setTargetWorkouts(null)
+    }
     useEffect(()=> {
       fetch('https://exercisedb.p.rapidapi.com/exercises/bodyPartList',{
           method: 'GET',
           headers: {
-              'X-RapidAPI-Key': 'd44ca9abf0msh20ad5caa52fd555p17b31djsn05d5cbbdd215',
+              'X-RapidAPI-Key': 'f9b7cb80aemsh9af6049dbb14119p1eef6djsnde28bb7c7c07',
               'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
          }
     })
@@ -34,7 +58,21 @@ const BodyPartsList = () => {
               }} 
             onMouseOver={(e)=> e.target.style.background = "#D3D3D3"} 
             onMouseOut={(e)=> e.target.style.background = "gray" } 
-            onFocus={e=> console.log(e.target.textContent)} >{item}</button>
+            onFocus={handleWorkouts} onBlur={handdleDelete}>{item}</button>
+            <button
+            style={{
+              fontSize:"15px",
+              background:"gray",
+              color:"white",
+              padding:"15px",
+              cursor:"pointer",
+              margin:"2px",
+              border:"none" 
+              }} 
+              onMouseOver={(e)=> e.target.style.background = "#D3D3D3"} 
+              onMouseOut={(e)=> e.target.style.background = "gray" } 
+              onClick={handdleDelete} >x</button>
+            {workout}
         </div>
       
       )
