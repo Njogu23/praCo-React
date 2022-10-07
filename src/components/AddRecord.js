2633fa57383e3ce4d21be0f3db12fd613e8428d7
George Njogu <sbeveme@gmail.com>
2022-10-08 01:29:43 +0300

    addrecord finalized

--- src/components/AddRecord.js
+++ src/components/AddRecord.js
@@ -1,46 +1,17 @@
-import React, {useState} from "react";
+import React from "react";
 
-const AddRecord = () => {
-    
-    const [record, setRecord] = useState({
-        date:"",
-        exercise:"",
-        target:"",
-        sets:"",
-        reps:""
-    })
-    const handleChange = (e) => {
-        setRecord([{
-            ...record,
-            [e.target.name]:e.target.value
-        }])
+const AddRecord = ({handleChange, handleSubmit, record}) => {
 
-
-    const handleAdd = (e) => {
-        e.preventDefault()
-        fetch("http://localhost:3000/records", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(record),
-          });
-    }     
-       
-    }
 
     return (
         <div >
-            <form>
-                <div className="">
-                    <input type="date" className="border border-danger bg-info text-dark" name="date"></input>
-                    <input type="text" placeholder="Add Exercise..." className="border border-danger bg-info text-dark " name="exercise"></input>
-                    <input type="text" placeholder="Add Target..." className="border border-danger bg-info text-dark" name="target"></input>
-                    <input type="text" placeholder="Add Sets..." className="border border-danger bg-info text-dark" name="sets"></input>
-                    <input type="text" placeholder="Add Reps..." className="border border-danger bg-info text-dark" name="reps"></input>
-                    <button className="btn btn-primary mb-1">Add</button>
-                </div>
-                
+            <form onSubmit={handleSubmit}>
+                    <input type="date" className="border border-danger bg-info text-dark" name="date" value={record.date} onChange={handleChange}/>
+                    <input type="text" placeholder="Add Exercise..." className="border border-danger bg-info text-dark " name="exercise" value={record.exercise} onChange={handleChange}/>
+                    <input type="text" placeholder="Add Target..." className="border border-danger bg-info text-dark" name="target" value={record.target} onChange={handleChange}/>
+                    <input type="text" placeholder="Add Sets..." className="border border-danger bg-info text-dark" name="sets" value={record.sets} onChange={handleChange}/>
+                    <input type="text" placeholder="Add Reps..." className="border border-danger bg-info text-dark" name="reps" value={record.reps} onChange={handleChange}/>
+                    <button className="btn btn-primary mb-1" type="submit">Add</button>
             </form>
         </div>
        
