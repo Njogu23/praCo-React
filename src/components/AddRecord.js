0a12aae153d31ce386617d199f1c69adbb93ad3b
George Njogu <sbeveme@gmail.com>
2022-10-09 14:12:41 +0300

    date input edited

--- src/components/AddRecord.js
+++ src/components/AddRecord.js
@@ -6,7 +6,7 @@ const AddRecord = ({handleChange, handleSubmit, record}) => {
     return (
         <div >
             <form onSubmit={handleSubmit}>
-                    <input type="date" className="border border-danger bg-info text-dark" name="date" value={record.date} onChange={handleChange}/>
+                    <input type="date" placeholder="Date" className="border border-danger bg-info text-dark" name="date" value={record.date} onChange={handleChange}/>
                     <input type="text" placeholder="Add Exercise..." className="border border-danger bg-info text-dark " name="exercise" value={record.exercise} onChange={handleChange}/>
                     <input type="text" placeholder="Add Target..." className="border border-danger bg-info text-dark" name="target" value={record.target} onChange={handleChange}/>
                     <input type="text" placeholder="Add Sets..." className="border border-danger bg-info text-dark" name="sets" value={record.sets} onChange={handleChange}/>
